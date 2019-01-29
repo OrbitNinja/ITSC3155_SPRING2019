e22bef3b834807ed7fa5d31f81966dc6f3f10a4f
Tan <tdo13@uncc.edu>
2019-01-29 15:52:35 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,22 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  sumX = 0;
+  
+  arr.each do |x|
+    sumX += x;
+  end
+  
+  return sumX;
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
