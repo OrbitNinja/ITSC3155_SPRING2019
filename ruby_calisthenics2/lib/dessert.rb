c615ec669bb9d68df839d484d7b46fe96e291dde
Tan <tdo13@uncc.edu>
2019-02-21 03:02:06 +0000

    Ruby OOP - Part 1

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,13 +1,30 @@
 class Dessert
   # add code for setters and getters
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
+    
+    def name=(name)
+      @name = name
+    end
+    
+    def calories=(calories)
+      @calories = calories
+    end
+    
+    def name
+      @name
+    end
+    
+    def calories
+      @calories
+    end
   end
   def healthy?
-    # your code here
+    
   end
   def delicious?
-    # your code here
+    
   end
 end
 
