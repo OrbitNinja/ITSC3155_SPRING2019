9284a523429a809a2a1f8d7537e2f43332bd939b
Tan <tdo13@uncc.edu>
2019-02-26 15:50:17 +0000

    Ruby OO Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,26 @@
 # Ruby Basics Part 3
 
-class BookInStock
-# YOUR CODE HERE
+class BookInStock #attr_accessor :ISBN, :price
+    
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = price
+        
+        def isbn=(isbn)
+            @isbn = isbn
+        end
+        def price=(price)
+            @price = price
+        end
+        
+        def isbn
+            @isbn
+        end
+        def price
+            @price
+        end
+        
+        
+    end
+    
 end
