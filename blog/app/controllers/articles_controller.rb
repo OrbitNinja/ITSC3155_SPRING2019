d9da6209896bb2b3b179f1eb8ac021c0e30f7b1b
Tan <tdo13@uncc.edu>
2019-03-14 14:56:12 +0000

    Complete Create and Show

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(articles_params)
+        
+        @article.save 
+        redirect_to @article
     end
+    
+    def show
+       @article = Article.find(params[:id]) 
+    end
+    
 end
+
+private
+    def articles_params
+        params.require(:article).permit(:title, :text)
+    end
+    
